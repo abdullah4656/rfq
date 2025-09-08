24c9dfb44f7dd91dbee1d777aa9fb45dab75ffc8
Abdullah Yasin <abdullahyasin4656@gmail.com>
2025-09-09 00:26:13 +0500

    Describe what you changed

--- rfq_project/urls.py
+++ rfq_project/urls.py
@@ -6,8 +6,3 @@ urlpatterns = [
     path("", include("rfq_app.urls")),
 
 ]
-import debug_toolbar
-from django.conf import settings
-if settings.DEBUG:
-   
-    urlpatterns += [path("__debug__/", include(debug_toolbar.urls))]
